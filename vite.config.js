621dcc815613f4a9f6e5a8e8c068f3bf2eccebe7
許嘉玲 <stephanie@xujialingdeMacBook-Pro.local>
2026-06-08 01:39:43 +0800

    fix github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-project/',
-  plugins: [react()]
+  plugins: [react()],
+  base: '/my-project/'
   
 })
